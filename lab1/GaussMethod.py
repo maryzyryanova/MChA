f6d296bd08c3516f5f1d21aad78a32330703acc2
maryzyryanova <79420458+maryzyryanova@users.noreply.github.com>
2022-02-11 13:44:14 +0300

    def count_q

--- lab1/GaussMethod.py
+++ lab1/GaussMethod.py
@@ -22,13 +22,27 @@ b = np.array([
     [4, 2]
 ])
 
+#main function
 def main():
-    print(countA())
+    matrixA = countA()
+    print(matrixA)
+    k = 1
+    for i in 5:
+        for j in 5:
+            matrixA[i][j] = matrixA[i][j] - count_q(i, k, matrixA) 
+        k += 1
 
+#find matrix A
 def countA():
     B = 10 * C
-    B.shape == D.shape
-    A = B + D
+    B.shape == D.shape #check the equality of matrixes
+    A = B + D 
     return A
+
+#main koeff q
+def count_q(i, k, matrixA):
+    q = np.array()
+    q[i][k] = matrixA[i][k] / matrixA[k][k] #counting the main koeff
+    return q[i][k]
 
 main()
