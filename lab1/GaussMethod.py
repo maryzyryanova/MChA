938446276b292d00265bb83049455dfba2db54ab
maryzyryanova <79420458+maryzyryanova@users.noreply.github.com>
2022-02-12 16:04:00 +0300

    fixing revert stroke

--- lab1/GaussMethod.py
+++ lab1/GaussMethod.py
@@ -2,6 +2,7 @@
 import numpy as np
 
 #defining matrixes 
+b = np.array([4.2 for i in range(5)])
 C = np.array([
     [0.2, 0, 0.2, 0, 0], 
     [0, 0.2, 0, 0.2, 0], 
@@ -16,13 +17,6 @@ D = np.array([
     [0.67, 0.92, -0.53, 2.33, 0.81], 
     [0.81, 0.67, 0.92, -0.53, 2.33]
 ])
-b = np.array([
-    [4, 2], 
-    [4, 2],
-    [4, 2],
-    [4, 2],
-    [4, 2]
-])
 
 #main function
 def main():
@@ -30,7 +24,8 @@ def main():
     print(f"\nMatrix b: \n{b}")
     print(f"\nMatrix A: \n{matrixA}")  
     matrixA = straight_stroke(matrixA)
-    print(f"\nMatrix A after the straight stroke: \n{matrixA}\n")
+    print(f"\nMatrix A after the straight stroke: \n{matrixA}")
+    print(f"\nMatrix b after the straight stroke: \n{b}\n")
     invert_stroke(matrixA)
 
 #find matrix A
@@ -55,15 +50,15 @@ def straight_stroke(matrixA):
             q = count_q(i, j, matrixA)
             for k in range(5):
                 matrixA[j][k] = matrixA[j][k] - q * matrixA[i-1][k]
+                b[k] -= q * b[i]
     return matrixA
 
 #invert stroke (something wrong here)
 def invert_stroke(matrixA):
     x = [0 for i in range(5)] #the list of the koeffs
     for i in range(4, -1, -1):
-        x[i] = matrixA[i][i]/b[i]
-        for j in range(4, -1, -1):
-            x[i] = x[i] - matrixA[i][j] * x[j] / matrixA[i][i]
+        x[i] = b[i] - sum([matrixA[i][j] * x[j] for j in range(i+1, 4)])/matrixA[i][i]
+    print("The result:")
     for i in range(5):
         print(f"x[{i+1}] = {x[i]}")
 
