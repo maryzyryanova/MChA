2809d7a816814916eef5466bf57e8b78e32a9509
maryzyryanova <79420458+maryzyryanova@users.noreply.github.com>
2022-02-17 13:23:25 +0300

    testing

--- lab1/Main.py
+++ lab1/Main.py
@@ -4,6 +4,7 @@ import PartlyChoose as partly_choose
 import numpy as np
 
 #defining matrix
+A = np.array([])
 b = [4.2 for i in range(5)]
 C = np.array([
     [0.2, 0, 0.2, 0, 0],
@@ -20,22 +21,13 @@ D = np.array([
     [0.81, 0.67, 0.92, -0.53, 2.33]
 ])
 
-#count matrix A
-def countA(C, D):
-    B = 10 * C
-    print(f"\nMatrix C: \n{C}")
-    print(f"\nMatrix B: \n{B}")
-    print(f"\nMatrix D: \n{D}")
-    A = B + D 
-    return A
+A = 10*C + D
 
-A = countA(C, D)
-
-print("1. Using Gauss method:")
+print("\n1. Using Gauss method:")
 gauss_method.gaussMethod(A, b)
 
-print("2. Using method of choosing in columns: ")
+print("\n2. Using method of choosing in columns: ")
 whole_choose.mainElement(A, b)
 
-print("3. Using method of choosing in the whole matrix: ")
+print("\n3. Using method of choosing in the whole matrix: ")
 partly_choose.findMainElement(A, b)
