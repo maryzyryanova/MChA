2b0a8054c848cb4b2218b09157e115eacf7ef835
maryzyryanova <79420458+maryzyryanova@users.noreply.github.com>
2022-02-28 23:04:44 +0300

    editing method

--- lab3/shturm_method.py
+++ lab3/shturm_method.py
@@ -6,6 +6,14 @@ def shturm_row():
     '''Create a shturm row'''
     x = Symbol('x')
     y = x**3 - 6.4951 * x**2 - 31.2543 * x + 23.1782
-    print(y.diff())
+    output_row = []
+    output_row.append(y)
+    output_row.append(y.diff())
+    count = len(output_row)
+    for i in range(0, count, 1):
+        new_element = div(output_row[i], output_row[i+1])[1]
+        output_row.append(new_element)
+        count += 1
+    print(output_row)
 
 shturm_row()
