615bfca5b2848fec1000665205834022d042bee3
maryzyryanova <79420458+maryzyryanova@users.noreply.github.com>
2022-03-01 15:12:30 +0300

    fix bugs

--- lab3/shturm_method.py
+++ lab3/shturm_method.py
@@ -9,7 +9,7 @@ def shturm_row(y, x):
     output_row.append(y.diff())
     count = len(output_row)
     for i in range(0, count, 1):
-        new_element = div(output_row[i], output_row[i+1])[1]
+        new_element = -div(output_row[i], output_row[i+1])[1]
         output_row.append(new_element)
         count += 1
     return output_row
