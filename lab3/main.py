413259d39f03dfb3f6b9c8f2118406aae397d1a9
maryzyryanova <79420458+maryzyryanova@users.noreply.github.com>
2022-03-02 02:08:16 +0300

    end

--- lab3/main.py
+++ lab3/main.py
@@ -4,7 +4,6 @@ import shturm_method as sm
 import split_roots as sr
 import half_method as hm
 import chord_method as cm
-import newton_method as nm
 
 def main():
     x = Symbol('x')
@@ -13,10 +12,7 @@ def main():
     print(f"\nAmount of variables, using Shturm Method: {amount_of_variables}")    
     print("\n1. Half division method: ")
     hm.half_div()
-    print("\n2. Chord method: ")
     cm.chord_method()
-    print("\n3. Newton method: ")
-    nm.newtonsMethod(-10, 0)
     sr.plot()
 
 main()
