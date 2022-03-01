799508cbe4625cc1cf785a926c141bfb59c4f6ce
maryzyryanova <79420458+maryzyryanova@users.noreply.github.com>
2022-03-01 10:24:59 +0300

    find_n function

--- lab3/shturm_method.py
+++ lab3/shturm_method.py
@@ -2,9 +2,10 @@
 from sympy import * 
 import numpy as np
 
+x = Symbol('x')
+
 def shturm_row():
     '''Create a shturm row'''
-    x = Symbol('x')
     y = x**3 - 6.4951 * x**2 - 31.2543 * x + 23.1782
     output_row = []
     output_row.append(y)
@@ -14,6 +15,13 @@ def shturm_row():
         new_element = div(output_row[i], output_row[i+1])[1]
         output_row.append(new_element)
         count += 1
-    print(output_row)
+    return output_row
 
-shturm_row()
+def find_n(output_row, interval_border):
+    '''Find N(a) and N(b)'''
+    for i in range(output_row):
+        temp_element = output_row[i].subs(x, interval_border)
+
+current_row = shturm_row()
+find_n(current_row, -10)
+find_n(current_row, 10)
