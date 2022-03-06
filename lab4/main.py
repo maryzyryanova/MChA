70b5e714a991e15af16594159aaadc57ece37215
maryzyryanova <79420458+maryzyryanova@users.noreply.github.com>
2022-03-06 13:57:12 +0300

    find first approximation

--- lab4/main.py
+++ lab4/main.py
@@ -1,9 +1,13 @@
-from sympy import *
-import iterations_method as iterations
+import sympy
 
-x = Symbol('x')
-y = Symbol('y')
+(x, y) = sympy.symbols('x y')
 eps = 1e-4
-f1 = lambda x,y: tan(x*y + 0.1) - x
-f2 = lambda x,y: 0.8*x**x + 2*y**2 - 1
-iterations.plot(f1(x,y), f2(x,y))
+f1 = sympy.tan(x*y + 0.1) - x
+f2 = 0.8*x**x + 2*y**2 - 1
+
+plots = sympy.plot_implicit(sympy.Eq(f1, 0), (x, -2, 2), (y, -2, 2), line_color = "blue", show = False)
+plots.extend(sympy.plot_implicit(sympy.Eq(f2, 0), (x, -2, 2), (y, -2, 2), line_color = "red", show = False))
+plots.show()
+
+first_approximation = [0.2, 0.4]
+
