3f3d8439ccba8db404b97de6b94e1507a401767f
maryzyryanova <79420458+maryzyryanova@users.noreply.github.com>
2022-03-06 15:32:47 +0300

    simple iterations method

--- lab4/iterations_method.py
+++ lab4/iterations_method.py
@@ -1,4 +1,19 @@
 import numpy as np
-from sympy import *
-import matplotlib.pyplot as plt
-from matplotlib import cm
+import sympy
+
+def get_x(x, y):
+    return sympy.tan(x*y + 0.1) 
+
+def get_y(x, y):
+    return sympy.sqrt((1-0.8*x**2)/2)
+
+def simple_iterations(first_approximation, eps):
+    result_array = np.array(first_approximation)
+    temp = 1
+    while temp > eps:
+        new_x = np.copy(result_array)
+        new_x[0] = get_x(*new_x)
+        new_x[1] = get_y(*new_x)
+        temp = np.max(np.abs(result_array - new_x))
+        result_array = new_x
+    print(result_array)
