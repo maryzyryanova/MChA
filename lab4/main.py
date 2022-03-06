dc6f321adac2c55b9c3abcd19ab801ef947d9606
maryzyryanova <79420458+maryzyryanova@users.noreply.github.com>
2022-03-06 13:22:21 +0300

    plot

--- lab4/main.py
+++ lab4/main.py
@@ -1,8 +1,9 @@
-from sympy import * 
+from sympy import *
+import iterations_method as iterations
 
 x = Symbol('x')
 y = Symbol('y')
 eps = 1e-4
-f1 = tan(x*y + 0.1) - x
-f2 = 0.8*x**x + 2*y**2 - 1
-
+f1 = lambda x,y: tan(x*y + 0.1) - x
+f2 = lambda x,y: 0.8*x**x + 2*y**2 - 1
+iterations.plot(f1(x,y), f2(x,y))
