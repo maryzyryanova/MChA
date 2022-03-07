24ff954927d7e8f2d28513b2599f8e1c398996d9
maryzyryanova <79420458+maryzyryanova@users.noreply.github.com>
2022-03-07 15:54:59 +0300

    newton_method finished

--- lab4/iterations_method.py
+++ lab4/iterations_method.py
@@ -2,18 +2,25 @@ import numpy as np
 import sympy
 
 def get_x(x, y):
-    return sympy.tan(x*y + 0.1) 
+    return np.tan(x*y + 0.1) 
 
-def get_y(x, y):
-    return sympy.sqrt((1-0.8*x**2)/2)
+def get_y(x):
+    return np.sqrt((1-0.8*(x**2))/2)
 
 def simple_iterations(first_approximation, eps):
-    result_array = np.array(first_approximation)
+    result_array = np.asarray(first_approximation)
     temp = 1
+    iterations_counter = 0
     while temp > eps:
-        new_x = np.copy(result_array)
-        new_x[0] = get_x(*new_x)
-        new_x[1] = get_y(*new_x)
+        new_x = result_array.copy()
+        new_x[0], new_x[1] = get_x(result_array[0], result_array[1]), get_y(result_array[0])
         temp = np.max(np.abs(result_array - new_x))
-        result_array = new_x
-    print(result_array)
+        result_array = new_x.copy()
+        iterations_counter += 1
+    return result_array, iterations_counter
+
+def print_result(first_approximation, eps):
+    result_to_print, iterations_counter = simple_iterations(first_approximation, eps)
+    x_variable = int(result_to_print[0] * 10000)/10000
+    y_variable = int(result_to_print[1] * 10000)/10000
+    print(f"x = {x_variable}\ny = {y_variable}\nIterations: {iterations_counter}")
--- lab4/main.py
+++ lab4/main.py
@@ -1,14 +1,18 @@
 import sympy
 import iterations_method
+import newton_method
 
 (x, y) = sympy.symbols('x y')
-eps = 1e-4
-f1 = lambda x,y: sympy.tan(x*y + 0.1) - x
-f2 = lambda x,y: 0.8*x**x + 2*y**2 - 1
+eps = 0.0001
+f1 = sympy.tan(x*y + 0.1) - x
+f2 = 0.8*x**2 + 2*y**2 - 1
 
-plots = sympy.plot_implicit(sympy.Eq(f1(x,y), 0), (x, -2, 2), (y, -2, 2), line_color = "blue", show = False)
-plots.extend(sympy.plot_implicit(sympy.Eq(f2(x,y), 0), (x, -2, 2), (y, -2, 2), line_color = "red", show = False))
+plots = sympy.plot_implicit(sympy.Eq(f1, 0), (x, -2, 2), (y, -2, 2), line_color = "blue", show = False)
+plots.extend(sympy.plot_implicit(sympy.Eq(f2, 0), (x, -2, 2), (y, -2, 2), line_color = "red", show = False))
 plots.show()
 
-first_approximation = [0.2, 0.4]
-iterations_method.simple_iterations(first_approximation, eps)
+first_approximation = [0.35, 0.65]
+print("1. Simple iterations method: ")
+iterations_method.print_result(first_approximation, eps)
+print("2. Newton method: ")
+newton_method.print_result(first_approximation, eps)
